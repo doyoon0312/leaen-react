977c7bc1b96602541cdc04eef3097c81a5bce150
kimwonmyong <kimwonmyong@gmail.com>
2024-09-09 13:55:28 +0900

    24년 9월 24일

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Card from './commponents/Card';
+
 function App() {
   // logic
   console.log('app.js');
@@ -24,8 +26,55 @@ function App() {
     console.log('관리자 페이지 접근 불가능');
   }
 
+  // Default parmeter 매개변수
+
+  // 함수 정의
+  const greeting = (input = '고객') => {
+    return `${input}님, 반갑습니다!`;
+  };
+
+  // 함수 호출
+  console.log(greeting());
+
+  // 구조 분해 할당
+  const studentList = ['김원명', '김도윤'];
+  const [first = '조도우', second, third = '김현호'] = studentList;
+
+  // 객체 구조 분해 할당
+  const person = {
+    name1: '김원명',
+    age1: 35,
+    email1: 'kimwonmyong@gmail.com',
+  };
+
+  // const name = person.name
+  // const age = person.age
+  // const email = person.email
+
+  // const { name1, age1, email1 } = person;
+  console.log('🚀person:', person);
+
+  // name만 바꾸기
+  const personNew = { ...person, name1: '김도삼' };
+  console.log('🚀personNew:', personNew);
+
+  // 전개 구문 Spread Syntax
+  const studentList1 = ['김원명', '김도윤'];
+
+  const addStudent1 = () => {
+    const addStudent1 = [...studentList1, '김사랑'];
+    console.log('🚀addStudent1:', addStudent1);
+  };
+
+  addStudent1();
+
   //view
-  return <div>Hello, world!</div>;
+  return (
+    <>
+      <div>Hello, world!</div>
+      <Card />
+    </>
+  );
 }
 
 export default App;
---
+++ src/commponents/Card.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Card = () => {
+  return <div>sdsadasdas</div>;
+};
+
+export default Card;
